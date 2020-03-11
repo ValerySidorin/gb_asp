3a483dcab7e756fd828f54838dc46184f77f9287
Afsuga Arewreh <afsuga.arewreh@mail.ru>
2020-03-11 16:04:20 +0300

    Initialize

---
+++ efcore_test/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using efcore_test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcore_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext db;
+
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Employees.ToListAsync());
+        }
+
+        public async Task<IActionResult> Info(int? id)
+        {
+            if (id != null)
+            {
+                Employee emp = await db.Employees.FindAsync(id);
+                if (emp != null)
+                    return View(emp);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Employee employee)
+        {
+            db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Employee emp = await db.Employees.FindAsync(id);
+                if (emp != null)
+                    return View(emp);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee employee)
+        {
+            db.Entry(employee).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Employee emp = await db.Employees.FindAsync(id);
+                return View(emp);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Employee emp = new Employee(id.Value);
+                db.Entry(emp).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ efcore_test/Migrations/20200311114245_NewMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efcore_test.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "FirstName", table: "Employees");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
